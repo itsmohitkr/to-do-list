8739639b576c15d1f00b2bd5f8b08bbc00325b73
Mohit kumar <50844220+itsmohitkr@users.noreply.github.com>
2020-12-29 16:23:30 +0530

    title changed

--- script.js
+++ script.js
@@ -5,36 +5,37 @@ let btnSort = $('#btnSort')
 let btnCleanup = $('#btnCleanup')
 let inpNewTask = $('#inpNewTask')
 
-function addItem(){
-    if(inpNewTask.val()!=''){
-    let listitem=$('<li>',{
-        'class':'list-group-item',text:inpNewTask.val()
-    })
-    listitem.click(()=>{
-        listitem.toggleClass('done')
-    })
-    ulTasks.prepend(listitem)
-    inpNewTask.val('')
-}
+function addItem() {
+    if (inpNewTask.val() != '') {
+        let listitem = $('<li>', {
+            'class': 'list-group-item',
+            text: inpNewTask.val()
+        })
+        listitem.click(() => {
+            listitem.toggleClass('done')
+        })
+        ulTasks.prepend(listitem)
+        inpNewTask.val('')
+    }
 }
 
 $('#ulTasks.done').html('<span class="badge bg-success">Success</span>')
-function cleardone(){
-    
+
+function cleardone() {
+
     $('#ulTasks .done').remove()
 }
 
-function sorting(){
+function sorting() {
     $('#ulTasks .done').appendTo(ulTasks)
 }
 
 
-inpNewTask.keypress((e)=>{
-    if(e.which==13)addItem()
+inpNewTask.keypress((e) => {
+    if (e.which == 13) addItem()
 })
 btnAdd.click(addItem)
-btnReset.click(()=>inpNewTask.val(''))
+btnReset.click(() => inpNewTask.val(''))
 
 btnCleanup.click(cleardone)
 btnSort.click(sorting)
-
